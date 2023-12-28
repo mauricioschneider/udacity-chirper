12f96885919fa57a4aaaf6682909d86ebf94fb94
Mauricio Schneider <mauricio@schneider.xyz>
2023-12-28 14:37:44 -0800

    Load initial data on App component

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,6 +1,6 @@
 export const RECEIVE_USERS = "RECEIVE_USERS";
 
-export function recieveUsers(users) {
+export function receiveUsers(users) {
   return {
     type: RECEIVE_USERS,
     users,
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,17 @@
-const App = () => {
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { PropTypes } from "prop-types";
+import { handleInitialData } from "../actions/shared";
+
+const App = (props) => {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, []);
   return <div>Starter Code</div>;
 };
 
-export default App;
+App.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(App);
