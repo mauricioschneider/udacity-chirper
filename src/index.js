d3ab8f04aee84d6ce65778955e8f771d422b6001
Mauricio Schneider <mauricio@schneider.xyz>
2023-12-28 13:50:00 -0800

    Add first reducers.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
 import "./index.css";
 import App from "./components/App";
 
+const store = configureStore({ reducer });
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
